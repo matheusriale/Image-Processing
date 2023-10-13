4f26dfaa29edf038c0ddd3a01929db50632db404
Rafael Coelho <rafaelcgol@alu.ufc.br>
2023-10-13 00:43:15 -0300

    Fazendo os filtros de sobel e detecção por grad

--- convolution.js
+++ convolution.js
@@ -25,6 +25,18 @@ function convolution(tipo, n) {
     if (tipo == 5 && n == 5) convolutionSharpeningHighboost5x5();
     if (tipo == 5 && n == 7) convolutionSharpeningHighboost7x7();
     if (tipo == 5 && n == 9) convolutionSharpeningHighboost9x9();
+    if (tipo == 6 && n == 3) convolutionSobelx3x3();
+    if (tipo == 6 && n == 5) convolutionSobelx5x5();
+    if (tipo == 6 && n == 7) convolutionSobelx7x7();
+    if (tipo == 6 && n == 9) convolutionSobelx9x9();
+    if (tipo == 7 && n == 3) convolutionSobely3x3();
+    if (tipo == 7 && n == 5) convolutionSobely5x5();
+    if (tipo == 7 && n == 7) convolutionSobely7x7();
+    if (tipo == 7 && n == 9) convolutionSobely9x9();
+    if (tipo == 8 && n == 3) convolutionGradient3x3();
+    if (tipo == 8 && n == 5) convolutionGradient5x5();
+    if (tipo == 8 && n == 7) convolutionGradient7x7();
+    if (tipo == 8 && n == 9) convolutionGradient9x9();
 }
 
 // Filtragem pela mediana 3x3:
@@ -183,7 +195,7 @@ function convolutionGauss9x9() {
     convolution9x9(kernel, false);
 }
 
-// Filtro de aguçamento (nitidez) por Laplaciano 3x3:
+// Filtro de detecção de bordas Laplaciano 3x3:
 function convolutionLaplace3x3() {
     let kernel = [0, -1,  0,
                  -1,  4, -1,
@@ -191,7 +203,7 @@ function convolutionLaplace3x3() {
     convolution3x3(kernel, false);
 }
 
-// Filtro de aguçamento (nitidez) por Laplaciano 5x5:
+// Filtro de detecção de bordas Laplaciano 5x5:
 function convolutionLaplace5x5() {
     let kernel = [0,  0, -1,  0,  0,
                   0, -1, -2, -1,  0,
@@ -201,7 +213,7 @@ function convolutionLaplace5x5() {
     convolution5x5(kernel, false);
 }
 
-// Filtro de aguçamento (nitidez) por Laplaciano 7x7:
+// Filtro de detecção de bordas Laplaciano 7x7:
 function convolutionLaplace7x7() {
     let kernel = [0,  0, -1, -1, -1,  0,  0,
                   0, -1, -3, -3, -3, -1,  0,
@@ -213,7 +225,7 @@ function convolutionLaplace7x7() {
     convolution7x7(kernel, false);
 }
 
-// Filtro de aguçamento (nitidez) por Laplaciano 9x9:
+// Filtro de detecção de bordas Laplaciano 9x9:
 function convolutionLaplace9x9() {
     let kernel = [0,  0, -3, -2, -2, -2, -3,  0,  0,
                   0, -2, -3, -5, -5, -5, -3, -2,  0,
@@ -227,11 +239,12 @@ function convolutionLaplace9x9() {
     convolution9x9(kernel, false);
 }
 
+// Filtro de aguçamento (nitidez) por Laplaciano 3x3:
 function convolutionSharpeningLaplace3x3() {
-    let data2 = [...pixels.data];
+    let data2 = [...pixels.data];                       // Imagem original
     convolutionLaplace3x3();
-    let copypixels = pixels; //copiar valores, novo array
-    let data = copypixels.data;
+    let copypixels = pixels;
+    let data = copypixels.data;                         // Imagem depois do laplace
     for (let i = 0; i < data.length; i = i + 4){
         data[i] = data[i] + data2[i];
         data[i+1] = data[i+1] + data2[i+1];
@@ -242,11 +255,60 @@ function convolutionSharpeningLaplace3x3() {
     drawHistogram();
 }
 
+// Filtro de aguçamento (nitidez) por Laplaciano 5x5:
+function convolutionSharpeningLaplace5x5() {
+    let data2 = [...pixels.data];                       // Imagem original
+    convolutionLaplace5x5();
+    let copypixels = pixels;
+    let data = copypixels.data;                         // Imagem depois do laplace
+    for (let i = 0; i < data.length; i = i + 4){
+        data[i] = data[i] + data2[i];
+        data[i+1] = data[i+1] + data2[i+1];
+        data[i+2] = data[i+2] + data2[i+2];
+    }
+    context.putImageData(pixels, 0, 0, 0, 0, canvas.width, canvas.height);
+    getFrequencies();
+    drawHistogram();
+}
+
+// Filtro de aguçamento (nitidez) por Laplaciano 7x7:
+function convolutionSharpeningLaplace7x7() {
+    let data2 = [...pixels.data];                       // Imagem original
+    convolutionLaplace7x7();
+    let copypixels = pixels;
+    let data = copypixels.data;                         // Imagem depois do laplace
+    for (let i = 0; i < data.length; i = i + 4){
+        data[i] = data[i] + data2[i];
+        data[i+1] = data[i+1] + data2[i+1];
+        data[i+2] = data[i+2] + data2[i+2];
+    }
+    context.putImageData(pixels, 0, 0, 0, 0, canvas.width, canvas.height);
+    getFrequencies();
+    drawHistogram();
+}
+
+// Filtro de aguçamento (nitidez) por Laplaciano 9x9:
+function convolutionSharpeningLaplace9x9() {
+    let data2 = [...pixels.data];                       // Imagem original
+    convolutionLaplace9x9();
+    let copypixels = pixels;
+    let data = copypixels.data;                         // Imagem depois do laplace
+    for (let i = 0; i < data.length; i = i + 4){
+        data[i] = data[i] + data2[i];
+        data[i+1] = data[i+1] + data2[i+1];
+        data[i+2] = data[i+2] + data2[i+2];
+    }
+    context.putImageData(pixels, 0, 0, 0, 0, canvas.width, canvas.height);
+    getFrequencies();
+    drawHistogram();
+}
+
+// Filtro de aguçamento (nitidez) por highboost 3x3:
 function convolutionSharpeningHighboost3x3() {
-    let data2 = [...pixels.data];
+    let data2 = [...pixels.data];                        // Imagem original
     convolutionGauss3x3();
-    let copypixels = pixels; //copiar valores, novo array
-    let data = copypixels.data;
+    let copypixels = pixels;
+    let data = copypixels.data;                          // Imagem depois do gauss
     for (let i = 0; i < data.length; i = i + 4){
         data[i] = data2[i] + 3*(data2[i] - data[i]);
         data[i+1] = data2[i+1] + 3*(data2[i+1] - data[i+1]);
@@ -255,4 +317,178 @@ function convolutionSharpeningHighboost3x3() {
     context.putImageData(pixels, 0, 0, 0, 0, canvas.width, canvas.height);
     getFrequencies();
     drawHistogram();
+}
+
+// Filtro de aguçamento (nitidez) por highboost 5x5:
+function convolutionSharpeningHighboost5x5() {
+    let data2 = [...pixels.data];                        // Imagem original
+    convolutionGauss5x5();
+    let copypixels = pixels;
+    let data = copypixels.data;                          // Imagem depois do gauss
+    for (let i = 0; i < data.length; i = i + 4){
+        data[i] = data2[i] + 3*(data2[i] - data[i]);
+        data[i+1] = data2[i+1] + 3*(data2[i+1] - data[i+1]);
+        data[i+2] = data2[i+2] + 3*(data2[i+2] - data[i+2]);
+    }
+    context.putImageData(pixels, 0, 0, 0, 0, canvas.width, canvas.height);
+    getFrequencies();
+    drawHistogram();
+}
+
+// Filtro de aguçamento (nitidez) por highboost 7x7:
+function convolutionSharpeningHighboost7x7() {
+    let data2 = [...pixels.data];                        // Imagem original
+    convolutionGauss7x7();
+    let copypixels = pixels;
+    let data = copypixels.data;                          // Imagem depois do gauss
+    for (let i = 0; i < data.length; i = i + 4){
+        data[i] = data2[i] + 3*(data2[i] - data[i]);
+        data[i+1] = data2[i+1] + 3*(data2[i+1] - data[i+1]);
+        data[i+2] = data2[i+2] + 3*(data2[i+2] - data[i+2]);
+    }
+    context.putImageData(pixels, 0, 0, 0, 0, canvas.width, canvas.height);
+    getFrequencies();
+    drawHistogram();
+}
+
+// Filtro de aguçamento (nitidez) por highboost 9x9:
+function convolutionSharpeningHighboost9x9() {
+    let data2 = [...pixels.data];                        // Imagem original
+    convolutionGauss9x9();
+    let copypixels = pixels;
+    let data = copypixels.data;                          // Imagem depois do gauss
+    for (let i = 0; i < data.length; i = i + 4){
+        data[i] = data2[i] + 3*(data2[i] - data[i]);
+        data[i+1] = data2[i+1] + 3*(data2[i+1] - data[i+1]);
+        data[i+2] = data2[i+2] + 3*(data2[i+2] - data[i+2]);
+    }
+    context.putImageData(pixels, 0, 0, 0, 0, canvas.width, canvas.height);
+    getFrequencies();
+    drawHistogram();
+}
+
+// Filtro de detecção de bordas Sobel x 3x3:
+function convolutionSobelx3x3() {
+    let kernel = [-1, 0, 1,
+                  -2, 0, 2,
+                  -1, 0, 1];
+    convolution3x3(kernel, false);
+}
+
+// Filtro de detecção de bordas Sobel y 3x3:
+function convolutionSobely3x3() {
+    let kernel = [-1, -2, -1,
+                   0,  0,  0,
+                   1,  2,  1];
+    convolution3x3(kernel, false);
+}
+
+// Filtro de detecção de bordas Sobel x 5x5:
+function convolutionSobelx5x5() {
+    let kernel = [-2, -1, 0, 1, 2,
+                  -2, -1, 0, 1, 2,
+                  -4, -2, 0, 2, 4,
+                  -2, -1, 0, 1, 2,
+                  -2, -1, 0, 1, 2];
+    convolution5x5(kernel, false);
+}
+
+// Filtro de detecção de bordas Sobel y 5x5:
+function convolutionSobely5x5() {
+    let kernel = [-2, -2, -4, -2, -2,
+                  -1, -1, -2, -1, -1,
+                   0,  0,  0,  0,  0,
+                   1,  1,  2,  1,  1,
+                   2,  2,  4,  2,  2,];
+    convolution5x5(kernel, false);
+}
+
+// Filtro de detecção de bordas Sobel x 7x7:
+function convolutionSobelx7x7() {
+    let kernel = [-3, -2, -1, 0, 1, 2, 3,
+                  -3, -2, -1, 0, 1, 2, 3,
+                  -3, -2, -1, 0, 1, 2, 3,
+                  -6, -4, -2, 0, 2, 4, 6,
+                  -3, -2, -1, 0, 1, 2, 3,
+                  -3, -2, -1, 0, 1, 2, 3,
+                  -3, -2, -1, 0, 1, 2, 3,];
+    convolution7x7(kernel, false);
+}
+
+// Filtro de detecção de bordas Sobel y 7x7:
+function convolutionSobely7x7() {
+    let kernel = [-3, -3, -3, -6, -3, -3, -3,
+                  -2, -2, -2, -4, -2, -2, -2,
+                  -1, -1, -1, -2, -1, -1, -1,
+                   0,  0,  0,  0,  0,  0,  0,
+                   1,  1,  1,  2,  1,  1,  1,
+                   2,  2,  2,  4,  2,  2,  2,
+                   3,  3,  3,  6,  3,  3,  3];
+    convolution7x7(kernel, false);
+}
+
+// Filtro de detecção de bordas Sobel x 9x9:
+function convolutionSobelx9x9() {
+    let kernel = [-4, -3, -2, -1, 0, 1, 2, 3, 4,
+                  -4, -3, -2, -1, 0, 1, 2, 3, 4,
+                  -4, -3, -2, -1, 0, 1, 2, 3, 4,
+                  -4, -3, -2, -1, 0, 1, 2, 3, 4,
+                  -8, -6, -4, -2, 0, 2, 4, 6, 8,
+                  -4, -3, -2, -1, 0, 1, 2, 3, 4,
+                  -4, -3, -2, -1, 0, 1, 2, 3, 4,
+                  -4, -3, -2, -1, 0, 1, 2, 3, 4,
+                  -4, -3, -2, -1, 0, 1, 2, 3, 4];
+    convolution9x9(kernel, false);
+}
+
+// Filtro de detecção de bordas Sobel y 9x9:
+function convolutionSobely9x9() {
+    let kernel = [-4, -4, -4, -4, -8, -4, -4, -4, -4, 
+                  -3, -3, -3, -3, -6, -3, -3, -3, -3,
+                  -2, -2, -2, -2, -4, -2, -2, -2, -2,
+                  -1, -1, -1, -1, -2, -1, -1, -1, -1,
+                   0,  0,  0,  0,  0,  0,  0,  0,  0,
+                   1,  1,  1,  1,  2,  1,  1,  1,  1,
+                   2,  2,  2,  2,  4,  2,  2,  2,  2,
+                   3,  3,  3,  3,  6,  3,  3,  3,  3,
+                   4,  4,  4,  4,  8,  4,  4,  4,  4];
+    convolution9x9(kernel, false);
+}
+
+// Filtro de detecção de bordas por gradiente 3x3:
+function convolutionGradient3x3() {
+    let data2 = [...pixels.data];                        // Imagem original
+    convolutionSobelx3x3();
+    let data3 = [...pixels.data];                        // Imagem depois do sobel x
+    let copypixels = pixels;
+    let data = copypixels.data;                          
+    for (let i = 0; i < data.length; i = i + 4){
+        data[i] = data2[i];
+        data[i+1] = data2[i+1];
+        data[i+2] = data2[i+2];
+    }
+    convolutionSobely3x3();
+    for (let i = 0; i < data.length; i = i + 4){
+        data[i] = data[i] + data3[i];
+        data[i+1] = data[i+1] + data3[i+1];
+        data[i+2] = data[i+2] + data3[i+2];
+    }
+}
+
+// Filtro de detecção de bordas por gradiente 5x5:
+function convolutionGradient5x5() {
+    convolutionSobelx5x5();
+    convolutionSobely5x5();
+}
+
+// Filtro de detecção de bordas por gradiente 7x7:
+function convolutionGradient7x7() {
+    convolutionSobelx7x7();
+    convolutionSobely7x7();
+}
+
+// Filtro de detecção de bordas por gradiente 9x9:
+function convolutionGradient9x9() {
+    convolutionSobelx9x9();
+    convolutionSobely9x9();
 }
