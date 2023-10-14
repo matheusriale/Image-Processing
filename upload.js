d464948213a897bea835d9e6ff0a8902973c6456
Felipesilver <felipe.souza94@alu.ufc.br>
2023-10-14 15:46:33 -0300

    Concluindo a escala pelo proximo

--- upload.js
+++ upload.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("image"); // criar canvas (representa desenho)
 var context = canvas.getContext("2d");         // estrutura que recebe dados para desenho
 var pixels = undefined;
+var original_width = undefined;
+var original_height = undefined;
 var image = new Image();
 var original_copy = pixels;
 
@@ -13,6 +15,8 @@ function upload(event){       // função chamada
     image.addEventListener("load",()=>{   // Força tag a ter certo comportamento
         canvas.width = 500;
         canvas.height = (500/image.width) *image.height;
+        original_width = canvas.width;
+        original_height = canvas.height;
         context.drawImage(image,0,0,canvas.width,canvas.height); // Desenhar imagem dentro do contexto
         pixels  = context.getImageData(0,0,canvas.width,canvas.height); // Captura pixels da imagem
         original_copy = [...pixels.data]
@@ -26,8 +30,10 @@ function upload(event){       // função chamada
 
 
 function undo(){
-    context.drawImage(image,0,0,canvas.width,canvas.height)
-    pixels  = context.getImageData(0,0,canvas.width,canvas.height);
+    canvas.width = original_width;
+    canvas.height = original_height;
+    context.drawImage(image,0,0,original_width,original_height);
+    pixels = context.getImageData(0,0,original_width,original_height);
     getFrequencies();
     drawHistogram();
 }
