fb6da2b36fa0844b9ce90c57a4ffbd2ac696cf46
Felipesilver <felipe.souza94@alu.ufc.br>
2023-10-14 17:24:25 -0300

    Concluindo transformações de escala

---
+++ spacialTransformations.js
@@ -0,0 +1,132 @@
+
+// Aplica a transformação de escala na imagem, interpolando pelo mais próximo:
+function scaleNearest(ratio) {
+
+    // Pixels da imagem original:
+    let data2 = [...pixels.data];
+
+    // Calculando a largura e altura da nova imagem:
+    let larguraOriginal = canvas.width;
+    let alturaOriginal = canvas.height;
+
+    let larguraNova = ratio * larguraOriginal;
+    let alturaNova = ratio * alturaOriginal;
+
+    // Atualizando o tamanho do canvas de exibição:
+    canvas.width = larguraNova;
+    canvas.height = alturaNova;
+
+    // Criando um novo objeto ImageData:
+    const imageData = context.createImageData(larguraNova, alturaNova);
+    let data3 = imageData.data;
+
+    // Laço que percorre a nova imagem preenchendo com base na anterior:
+    for (i = 0; i < data3.length; i = i + 4) {
+
+        // Calculando a linha e coluna do elemento atual da nova imagem:
+        let linhaAtual = Math.floor( i / (larguraNova*4) );
+        let colunaAtual = Math.round( (i % (larguraNova*4)) / 4 );
+
+        // Interpolando a linha e coluna correspondente na imagem original:
+        let linhaNaOriginal = Math.round( linhaAtual / ratio );
+        let colunaNaOriginal = Math.round( colunaAtual / ratio );
+
+        // Calculando o índice na imagem original:
+        let i2 = linhaNaOriginal*larguraOriginal*4 + colunaNaOriginal*4;
+
+        // Atribuindo o valor do pixel:
+        data3[i] = data2[i2];
+        data3[i+1] = data2[i2+1];
+        data3[i+2] = data2[i2+2];
+        data3[i+3] = data2[i2+3];
+    }
+
+    context.putImageData(imageData, 0, 0);
+    pixels = context.getImageData(0,0,canvas.width,canvas.height);
+    getFrequencies();
+    drawHistogram();
+}
+
+// Aplica a transformação de escala na imagem, interpolando pelo mais próximo:
+function scaleBilinear(ratio) {
+
+    // Pixels da imagem original:
+    let data2 = [...pixels.data];
+
+    // Calculando a largura e altura da nova imagem:
+    let larguraOriginal = canvas.width;
+    let alturaOriginal = canvas.height;
+
+    let larguraNova = ratio * larguraOriginal;
+    let alturaNova = ratio * alturaOriginal;
+
+    // Atualizando o tamanho do canvas de exibição:
+    canvas.width = larguraNova;
+    canvas.height = alturaNova;
+
+    // Criando um novo objeto ImageData:
+    const imageData = context.createImageData(larguraNova, alturaNova);
+    let data3 = imageData.data;
+
+    // Laço que percorre a nova imagem preenchendo com base na anterior:
+    for (i = 0; i < data3.length; i = i + 4) {
+
+        // Calculando a linha e coluna do elemento atual da nova imagem:
+        let linhaAtual = Math.floor( i / (larguraNova*4) );
+        let colunaAtual = Math.round( (i % (larguraNova*4)) / 4 );
+
+        // Interpolando a linha e coluna correspondente na imagem original:
+        let l1 = Math.floor( linhaAtual / ratio );
+        let l2 = Math.ceil( linhaAtual / ratio );
+        let c1 = Math.floor( colunaAtual / ratio );
+        let c2 = Math.ceil( colunaAtual / ratio );
+
+        let deltax = (colunaAtual / ratio) - c1;
+        let deltay = (linhaAtual / ratio) - l1;
+
+        // Calculando os índices na imagem original:
+        let i1 = l1*larguraOriginal*4 + c1*4;
+        let i2 = l1*larguraOriginal*4 + c2*4;
+        let i3 = l2*larguraOriginal*4 + c1*4;
+        let i4 = l2*larguraOriginal*4 + c2*4;
+
+        // Calculando o r:
+        // Interpolando em x:
+        rInt1 = data2[i1] * (1-deltax) + data2[i2] * deltax;
+        rInt2 = data2[i3] * (1-deltax) + data2[i4] * deltax;
+        // Interpolando em y:
+        rFinal = rInt1 * (1-deltay) + rInt2 * deltay;
+
+        // Calculando o g:
+        // Interpolando em x:
+        gInt1 = data2[i1+1] * (1-deltax) + data2[i2+1] * deltax;
+        gInt2 = data2[i3+1] * (1-deltax) + data2[i4+1] * deltax;
+        // Interpolando em y:
+        gFinal = gInt1 * (1-deltay) + gInt2 * deltay;
+
+        // Calculando o b:
+        // Interpolando em x:
+        bInt1 = data2[i1+2] * (1-deltax) + data2[i2+2] * deltax;
+        bInt2 = data2[i3+2] * (1-deltax) + data2[i4+2] * deltax;
+        // Interpolando em y:
+        bFinal = bInt1 * (1-deltay) + bInt2 * deltay;
+
+        // Calculando o alpha:
+        // Interpolando em x:
+        aInt1 = data2[i1+3] * (1-deltax) + data2[i2+3] * deltax;
+        aInt2 = data2[i3+3] * (1-deltax) + data2[i4+3] * deltax;
+        // Interpolando em y:
+        aFinal = aInt1 * (1-deltay) + aInt2 * deltay;
+
+        // Atribuindo o valor do pixel:
+        data3[i] = Math.round( rFinal );
+        data3[i+1] = Math.round( gFinal );
+        data3[i+2] = Math.round( bFinal );
+        data3[i+3] = Math.round( aFinal );
+    }
+
+    context.putImageData(imageData, 0, 0);
+    pixels = context.getImageData(0,0,canvas.width,canvas.height);
+    getFrequencies();
+    drawHistogram();
+}
