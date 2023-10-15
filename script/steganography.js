3928fd00b29dedf298ef3c4188d79adf286ab885
Matheus Ribeiro <matheusriale@alu.ufc.br>
2023-10-14 23:59:19 -0300

    steganography fix

--- script/steganography.js
+++ script/steganography.js
@@ -27,6 +27,7 @@ function decimalToBinary(number) {
 }
 
 function encrypt() {
+  const data = pixels.data;
   const msg = message.value;
   let msg_len = msg.length;
   let img_counter = 0;
@@ -42,25 +43,28 @@ function encrypt() {
     }
 
     for (let j = 0; j < binList.length; j++) {
-      if (original_copy[img_counter + j] % 2 == 1 && binList[j] == 1) {
+      if (data[img_counter + j] % 2 == 1 && binList[j] == 1) {
         continue;
-      } else if (original_copy[img_counter + j] % 2 == 1 && binList[j] == 0) {
-        original_copy[img_counter + j] = original_copy[img_counter + j] - 1;
-      } else if (original_copy[img_counter + j] % 2 == 0 && binList[j] == 0) {
+      } else if (data[img_counter + j] % 2 == 1 && binList[j] == 0) {
+        data[img_counter + j] = data[img_counter + j] - 1;
+      } else if (data[img_counter + j] % 2 == 0 && binList[j] == 0) {
         continue;
-      } else if (original_copy[img_counter + j] % 2 == 0 && binList[j] == 1) {
-        original_copy[img_counter + j] = original_copy[img_counter + j] + 1;
+      } else if (data[img_counter + j] % 2 == 0 && binList[j] == 1) {
+        data[img_counter + j] = data[img_counter + j] + 1;
       }
     }
     img_counter = img_counter + 7;
   }
   for (let j = 0; j < 7; j++) {
-    if (original_copy[img_counter + j] % 2 == 1) {
-      original_copy[img_counter + j] = original_copy[img_counter + j] - 1;
-    } else if (original_copy[img_counter + j] % 2 == 0) {
+    if (data[img_counter + j] % 2 == 1) {
+      data[img_counter + j] = data[img_counter + j] - 1;
+    } else if (data[img_counter + j] % 2 == 0) {
       continue;
     }
   }
+  context.putImageData(pixels, 0, 0, 0, 0, canvas.width, canvas.height);
+  getFrequencies();
+  drawHistogram();
   message.value = "";
   closeModal();
 }
@@ -71,20 +75,21 @@ function decrypt() {
   let msg = "";
   let char_bin = 0;
   let next_char = "";
+  const data = pixels.data;
   
   let reset_counter = false;
-  for (let i = 0; i < original_copy.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     reset_counter = false;
-    char_bin = char_bin + (original_copy[i] % 2) * counter;
+    char_bin = char_bin + (data[i] % 2) * counter;
     if (
-      original_copy[i] % 2 == 0 &&
+      data[i] % 2 == 0 &&
       counter == 0 && //1//bom por pra testar, se nao analisa imagem toda
-      original_copy[i + 1] % 2 == 0 &&
-      original_copy[i + 2] % 2 == 0 &&
-      original_copy[i + 3] % 2 == 0 &&
-      original_copy[i + 4] % 2 == 0 &&
-      original_copy[i + 5] % 2 == 0 &&
-      original_copy[i + 6] % 2 == 0
+      data[i + 1] % 2 == 0 &&
+      data[i + 2] % 2 == 0 &&
+      data[i + 3] % 2 == 0 &&
+      data[i + 4] % 2 == 0 &&
+      data[i + 5] % 2 == 0 &&
+      data[i + 6] % 2 == 0
     ) {
       counter = Math.pow(10, 6);
       let decimal_code = parseInt(char_bin, 2);
