30ab6a3dad8a9e971a3b2f5d9c10323ae8a34342
Matheus Ribeiro <matheusriale@alu.ufc.br>
2023-11-29 16:35:32 -0300

    chrominanceLuminance

--- script/compression.js
+++ script/compression.js
@@ -21,10 +21,52 @@ function runLength () {
         }
     }
 
+    // for (let i = 0; i < data.length; i += 1) {
+    //     // Verifica se o próximo elemento é igual ao atual
+    //     if (data[i] === data[i + 1] && countr < 255 ){
+    //         countr++;
+    //     } else {
+    //         // Se não for igual, adiciona o elemento atual e a contagem ao vetor comprimido
+    //         compressedImage.push(data[i], countr);
+    //         countr = 1; // Reinicia a contagem para o próximo elemento
+    //     }
+    // }
+
     compressedImageUint8 = new Uint8ClampedArray(compressedImage);
     console.log("Imagem Comprimida: " + compressedImageUint8);
     console.log(compressedImageUint8);
 }
+
+function chrominanceLuminance(){
+    let data = pixels.data;
+    let hsvImage = [];
+    for(let i = 0; i<data.length;i+=4){ 
+        hsvPixel = rgbToHSV(data[i],data[i+1],data[i+2]);
+        hsvImage.push(hsvPixel[0],hsvPixel[1],hsvPixel[2]); //alpha?
+        hsvPixel = [];
+    }
+    //console.log(hsvImage)//imagem com HSV, no formato: [h1,s1,v1,h2,s2,v2,...,hn,sn,vn]
+
+    // Aplicar hn = (h1+h2)/2, sn = (s1+s2)/2
+    let compressedHsv = [];
+    for(let i = 0; i <hsvImage.length;i+=6){
+        let hn = (hsvImage[i]+hsvImage[i+3])/2;
+        let sn = (hsvImage[i+1]+hsvImage[i+4])/2;
+        let x = 360/255;
+        hn = hn/x // garantir que hn esteja dentro do intervalo 0 - 255
+        compressedHsv.push(Math.round(hn),Math.round(sn),Math.round(hsvImage[i+5]),Math.round(hsvImage[i+6]))
+    }
+    //console.log("Compressed HSV",compressedHsv)
+    compressedHSVUint8 = new Uint8ClampedArray(compressedHsv)
+    //console.log(compressedHSVUint8)
+    return compressedHSVUint8
+}
+
+function undoChrominanceLuminance(){
+    return
+}
+
+
 
 function downloadCompressed() {
 
@@ -34,9 +76,7 @@ function downloadCompressed() {
     link.download = "compressed.mr";
     link.click();
 }
-
 function uploadCompressed (event) {
-
     const fileList = event.target.files;
 
     var fr = new FileReader();
