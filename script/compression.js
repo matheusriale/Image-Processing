43ed676dd3c6c0b2125817f0def8bcba18068343
Matheus Ribeiro <matheusriale@alu.ufc.br>
2023-11-30 13:48:15 -0300

    full compress and decompress

--- script/compression.js
+++ script/compression.js
@@ -1,71 +1,83 @@
 
 let compressedImage = [];
+let compImageCL = [];
 let compressedImageUint8;
 let decompressedImage = [];
 let decompressedImageUint8;
+let decompressedImageRL = []
+let compressedHSVUint8;
 var datac;
 
 function runLength () {
 
-    let data = pixels.data;
+    //let data = pixels.data;
     
     let countr = 1;
-    for (let i = 0; i < data.length; i += 4) {
+    for (let i = 0; i < compImageCL.length; i += 6) {
         // Verifica se o próximo elemento é igual ao atual
-        if (data[i] === data[i + 4] && data[i+1] === data[i+1 + 4] && data[i+2] === data[i+2 + 4] && countr < 255) {
+        if (compImageCL[i] === compImageCL[i + 6] && compImageCL[i+1] === compImageCL[i+1 + 6] && compImageCL[i+2] === compImageCL[i+2 + 6] && compImageCL[i+3] === compImageCL[i+3 + 6] && compImageCL[i+4] === compImageCL[i+4 + 6] && compImageCL[i+5] === compImageCL[i+5 + 6] && countr < 255) {
             countr++;
         } else {
             // Se não for igual, adiciona o elemento atual e a contagem ao vetor comprimido
-            compressedImage.push(data[i], data[i+1], data[i+2], data[i+3], countr);
+            compressedImage.push(compImageCL[i], compImageCL[i+1], compImageCL[i+2], compImageCL[i+3],compImageCL[i+4],compImageCL[i+5], countr);
             countr = 1; // Reinicia a contagem para o próximo elemento
         }
     }
 
-    // for (let i = 0; i < data.length; i += 1) {
-    //     // Verifica se o próximo elemento é igual ao atual
-    //     if (data[i] === data[i + 1] && countr < 255 ){
-    //         countr++;
-    //     } else {
-    //         // Se não for igual, adiciona o elemento atual e a contagem ao vetor comprimido
-    //         compressedImage.push(data[i], countr);
-    //         countr = 1; // Reinicia a contagem para o próximo elemento
-    //     }
-    // }
-
     compressedImageUint8 = new Uint8ClampedArray(compressedImage);
-    console.log("Imagem Comprimida: " + compressedImageUint8);
+    console.log("Imagem Comprimida run length: " + compressedImageUint8);
     console.log(compressedImageUint8);
 }
+
+
 
 function chrominanceLuminance(){
     let data = pixels.data;
     let hsvImage = [];
     for(let i = 0; i<data.length;i+=4){ 
         hsvPixel = rgbToHSV(data[i],data[i+1],data[i+2]);
-        hsvImage.push(hsvPixel[0],hsvPixel[1],hsvPixel[2]); //alpha?
+        hsvImage.push(hsvPixel[0],hsvPixel[1],hsvPixel[2],data[i+3]);
         hsvPixel = [];
     }
     //console.log(hsvImage)//imagem com HSV, no formato: [h1,s1,v1,h2,s2,v2,...,hn,sn,vn]
-
     // Aplicar hn = (h1+h2)/2, sn = (s1+s2)/2
-    let compressedHsv = [];
-    for(let i = 0; i <hsvImage.length;i+=6){
-        let hn = (hsvImage[i]+hsvImage[i+3])/2;
-        let sn = (hsvImage[i+1]+hsvImage[i+4])/2;
-        let x = 360/255;
+    //let compressedHsv = [];
+    let x = 360/255;
+    for(let i = 0; i <hsvImage.length;i+=8){
+        let hn = (hsvImage[i]+hsvImage[i+4])/2;
+        let sn = (hsvImage[i+1]+hsvImage[i+5])/2;
         hn = hn/x // garantir que hn esteja dentro do intervalo 0 - 255
-        compressedHsv.push(Math.round(hn),Math.round(sn),Math.round(hsvImage[i+5]),Math.round(hsvImage[i+6]))
+        compImageCL.push(Math.round(hn),Math.round(sn),Math.round(hsvImage[i+2]),hsvImage[i+3],Math.round(hsvImage[i+6]),hsvImage[i+7])
     }
-    //console.log("Compressed HSV",compressedHsv)
-    compressedHSVUint8 = new Uint8ClampedArray(compressedHsv)
-    //console.log(compressedHSVUint8)
-    return compressedHSVUint8
+    console.log("Compressed HSV chrominance luminance:",compImageCL)
+
+    //compressedImageUint8 = new Uint8ClampedArray(compImageCL)
+    //console.log(compressedImageUint8)
 }
 
 function undoChrominanceLuminance(){
-    return
-}
+    //decompressedImage
+    let x = 360/255;
+    for(let i = 0;i<decompressedImageRL.length;i+=6){
+        let rgb_values1 = hsvToRGB(decompressedImageRL[i]*x,decompressedImageRL[i+1],decompressedImageRL[i+2])
+        let rgb_values2 = hsvToRGB(decompressedImageRL[i]*x,decompressedImageRL[i+1],decompressedImageRL[i+4])
+        decompressedImage.push(Math.round(rgb_values1[0]),rgb_values1[1],rgb_values1[2],decompressedImageRL[i+3],Math.round(rgb_values2[0]),rgb_values2[1],rgb_values2[2],decompressedImageRL[i+5])
+        rgb_values1 = [];
+        rgb_values2 = [];
+    }
 
+    decompressedImageUint8 = new Uint8ClampedArray(decompressedImage);
+    console.log("Decompressed Image: " + decompressedImageUint8);
+    console.log(decompressedImageUint8);
+
+    var imgdt = new ImageData(decompressedImageUint8, canvas.width);
+    context.putImageData(imgdt, 0, 0);
+    pixels = context.getImageData(0, 0, canvas.width, canvas.height);
+    original_copy = [...pixels.data]
+    getFrequencies();
+    drawHistogram();
+    closeSubmenu();
+}
 
 
 function downloadCompressed() {
@@ -76,6 +88,7 @@ function downloadCompressed() {
     link.download = "compressed.mr";
     link.click();
 }
+
 function uploadCompressed (event) {
     const fileList = event.target.files;
 
@@ -91,22 +104,33 @@ function uploadCompressed (event) {
 
 function undoRunLength () {
 
-    for (let i = 0; i < datac.length; i += 5) {
-        for (let j = 0; j < datac[i+4]; j += 1) {
+    for (let i = 0; i < datac.length; i += 7) {
+        for (let j = 0; j < datac[i+6]; j += 1) {
             // Adiciona o elemento repetido a quantidade de vezes especificada pela contagem
-            decompressedImage.push(datac[i], datac[i+1], datac[i+2], datac[i+3]);
+            decompressedImageRL.push(datac[i], datac[i+1], datac[i+2], datac[i+3],datac[i+4],datac[i+5]);
         }
     }
 
-    decompressedImageUint8 = new Uint8ClampedArray(decompressedImage);
-    console.log("Imagem Descomprimida: " + decompressedImageUint8);
-    console.log(decompressedImageUint8);
+    // decompressedImageUint8 = new Uint8ClampedArray(decompressedImage);
+    console.log("Imagem Descomprimida runlength: " + decompressedImageRL);
+    console.log(decompressedImageRL);
 
-    var imgdt = new ImageData(decompressedImageUint8, canvas.width);
-    context.putImageData(imgdt, 0, 0);
-    pixels = context.getImageData(0, 0, canvas.width, canvas.height);
-    original_copy = [...pixels.data]
+    // var imgdt = new ImageData(decompressedImageUint8, canvas.width);
+    // context.putImageData(imgdt, 0, 0);
+    // pixels = context.getImageData(0, 0, canvas.width, canvas.height);
+    // original_copy = [...pixels.data]
     getFrequencies();
     drawHistogram();
     closeSubmenu();
+}
+
+
+function fullCompress(){
+    chrominanceLuminance()
+    runLength()
+}
+
+function fullDecompress(){
+    undoRunLength()
+    undoChrominanceLuminance()
 }
